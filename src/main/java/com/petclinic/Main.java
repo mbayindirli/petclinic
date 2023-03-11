8e6de4dbc35a6c717a6004b915803a83ac5349ea
MUSTAFA BAYINDIRLI <mustafa.bayindirli@fmsstech.com>
2023-03-11 12:40:29 +0300

    spring initializr

--- src/main/java/com/petclinic/Main.java
+++
@@ -1,7 +0,0 @@
-package com.petclinic;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/com/petclinic/Petclinic.java
@@ -0,0 +1,11 @@
+package com.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Petclinic {
+    public static void main(String[] args) {
+        SpringApplication.run(Petclinic.class, args);
+    }
+}
