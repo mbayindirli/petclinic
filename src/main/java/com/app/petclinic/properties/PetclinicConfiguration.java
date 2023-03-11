5f81420f31579128d717cb47703936b5d05182b9
MUSTAFA BAYINDIRLI <mustafa.bayindirli@fmsstech.com>
2023-03-11 16:19:58 +0300

    @PostConstruct and  @PreDestroy

--- src/main/java/com/app/petclinic/properties/PetclinicConfiguration.java
+++ src/main/java/com/app/petclinic/properties/PetclinicConfiguration.java
@@ -4,14 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Configuration
 public class PetclinicConfiguration {
     @Autowired
     PetclinicProperties petclinicProperties;
+    //uygulama ayağa kalkarken calış
     @PostConstruct
-    public void init() {
-        System.out.println("petclinic pro"+petclinicProperties.isDisplayOwnerWithPet());
+    public void init() throws Exception {
+        System.out.println("Inıt Metot PostConstruct : " + petclinicProperties.isDisplayOwnerWithPet());
+    }
+//uygulama kill edilirken yani stop olduğunda çalış
+    @PreDestroy
+    public void preinit() throws Exception {
+        System.out.println("Spring Konteyner Durdu ve preinit metodu çalıştı"+petclinicProperties.isDisplayOwnerWithPet());
     }
 
 }
